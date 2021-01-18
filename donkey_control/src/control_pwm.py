b0fa60cb57ed97b50905316f6824598e4c61ddba
kimsooyoung <tge1375@naver.com>
2021-01-18 22:00:38 +0900

    Local again

--- donkey_control/src/control_pwm.py
+++ donkey_control/src/control_pwm.py
@@ -36,18 +36,23 @@ class PCA9685:
         time.sleep(init_delay)  # "Tamiya TBLE-02" makes a little leap otherwise
 
         self.pulse = 340
+        self.running = True
 
     def set_pulse(self, pulse):
         self.pulse = pulse
 
-    def set_pwm(self):
+    def set_pwm(self, pulse):
         try:
-            self.pwm.set_pwm(self.channel, 0, int(self.pulse * self.pwm_scale))
+            self.pwm.set_pwm(self.channel, 0, int(pulse * self.pwm_scale))
         except:
-            self.pwm.set_pwm(self.channel, 0, int(self.pulse * self.pwm_scale))
+            self.pwm.set_pwm(self.channel, 0, int(pulse * self.pwm_scale))
 
-    def run(self):
-        self.set_pwm()
+    def run(self, angle):
+        self.set_pwm(pulse)
+
+    def update(self):
+        while self.running:
+            self.controller.set_pulse(self.pulse)
 
 
 class Vehicle(object):
@@ -59,10 +64,10 @@ class Vehicle(object):
         self._steering_servo = PCA9685(channel=1, busnum=1)
         rospy.loginfo("Steering Controler Awaked!!")
 
-        self._throttle_t = Thread(target=self._throttle.run, args=())
+        self._throttle_t = Thread(target=self._throttle.update, args=())
         self._throttle_t.daemon = True
 
-        self._steering_t = Thread(target=self._throttle.run, args=())
+        self._steering_t = Thread(target=self._throttle.update, args=())
         self._steering_t.daemon = True
 
 
@@ -76,8 +81,8 @@ class Vehicle(object):
         speed_pulse = msg.drive.speed
         steering_pulse = msg.drive.steering_angle
 
-        self._throttle.set_pulse(speed_pulse)
-        self._steering_servo.set_pulse(steering_pulse)
+        self._throttle.run(speed_pulse)
+        self._steering_servo.run(steering_pulse)
 
 
 if __name__ == "__main__":
